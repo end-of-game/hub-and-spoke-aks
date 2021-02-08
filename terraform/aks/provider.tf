dba4c2046b661f0ac88b5e9271d9a15d61c1cf98
Thomas Perelle <t.perelle@treeptik.fr>
2021-02-08 23:33:26 +0100

    Initial commit

---
+++ terraform/aks/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  
+  # Terraform backend configuration
+  backend "azurerm" {
+    resource_group_name   = "terraform-backends"
+    storage_account_name  = "terraform1612822914"
+    container_name        = "hubandspokeaks"
+    key                   = "terraform.tfstate"
+  }
+
+  # List required providers with version constraints
+  required_providers {
+    azurerm = {
+      source  = "azurerm"
+      version = "= 2.45.1"
+    }
+    azuread = {
+      source  = "azuread"
+      version = "= 1.3.0"
+    }
+  }
+}
+
+# Initialize the azurerm provider
+provider "azurerm" {
+  features {}
+}
