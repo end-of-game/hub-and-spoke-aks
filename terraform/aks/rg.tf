c1a783c5dfb7776d65a7a93c72a2d634a362c821
Abhishek Tyagi <atyagi@miceosoft.com>
2023-08-27 18:11:12 -0500

    delted foder

--- terraform/aks/rg.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "aks" {
-  name     = local.stack_name
-  location = var.location
-
-  tags = local.env_tags
-}
--- terraform/hub/rg.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "hub" {
-  name     = local.stack_name
-  location = var.location
-
-  tags = merge(var.default_tags)
-}
