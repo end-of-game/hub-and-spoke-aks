df1a607cab8e747931721f372db4d9f42118413e
Abhishek Tyagi <atyagi@miceosoft.com>
2023-08-27 18:51:08 -0500

    triggerflow

--- bookinfo/src/productpage/productpage.py
+++ bookinfo/src/productpage/productpage.py
@@ -10,7 +10,7 @@
 #
 #   Unless required by applicadble law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANYx KIND, either express or implied.
+#   WITHOUT WARRANTIES OR CONDITIOsNS OF ANYx KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
